8f11fa0c0d4ff3264c022635dc1c38f7b6c8718f
LordTejas <tejas5225@gmail.com>
2023-04-05 11:07:32 +0530

    Feature: The Base UI with action buttons for user list

--- frontend/src/components/UserList.js
+++ frontend/src/components/UserList.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
 import TextField from '@mui/material/TextField';
-import SearchIcon from '@mui/icons-material/Search';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -20,18 +20,23 @@ import IconButton from '@mui/material/IconButton';
 import Tooltip from '@mui/material/Tooltip';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Switch from '@mui/material/Switch';
-import DeleteIcon from '@mui/icons-material/Delete';
 import FilterListIcon from '@mui/icons-material/FilterList';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 import { visuallyHidden } from '@mui/utils';
 
 import './UserList.css';
-import config from '../config.json';;
+import config from '../config.json';import { Divider } from '@mui/material';
+;
 
 
 export default function UserList() {
 
     const [users, setUsers] = useState([]);
-    const headers = ["id", "name", "email", "role"];
+
+    // Using Set here, not an array
+    // It will increase some functionality, Due to O(1) Access time when we have much more load
+    const [selected, setSelected] = useState(new Set());
 
     const SearchBar = () => (
         <TextField
@@ -43,28 +48,114 @@ export default function UserList() {
         />
     );
 
-    const getTableCell = (value, alignment="center") => (
-        <TableCell align={alignment}>{value}</TableCell>
-    );
+    const isSelected = (id) => {
+        return selected.has(id);
+    }
 
-    const getTableRow = (user, headers) => (
+    const handleCheckboxClick = (e, id) => {
+
+        // Get Shallow copy of selected SET, Deep copy not needed
+        // Since, we only have 1 level deep
+        let newSelected = new Set(selected);
+
+        if (newSelected.has(id)) {
+            newSelected.delete(id);
+        } else {
+            newSelected.add(id);
+        }
+
+        console.log(newSelected);
+
+        // Swaps our newSelected to prop and updates
+        setSelected(newSelected);
+
+    }
+
+    const handleSelectAllClick = (event) => {
+        if (event.target.checked) {
+            // Fetch All user IDs and add it to our set
+            let newSelected = new Set(users.map((user) => user.id));
+            setSelected(newSelected);
+            return;
+        }
+
+        setSelected(new Set());
+      };
+    
+
+    const getUserTableRow = (user) => {
+        
+        const isItemSelected =  isSelected(user.id);
+        const labelId = `user-list-table-label-${user.id}`;
+        
+        return (
         <TableRow
-        key={user.id}>
-            {headers.map((h) => getTableCell(user[h]))}
-        </TableRow>
-    );
+        key={labelId}
+        role="checkbox"
+        aria-checked={isItemSelected}
+        selected={isItemSelected}
+        >
+            <TableCell 
+            align="center"
+            onClick={(e) => handleCheckboxClick(e, user.id)}
+            >
+                <Checkbox
+                color="primary"
+                checked={isItemSelected}
+                inputProps={{
+                    'aria-labelledby': {labelId},
+                }}
+                />
+            </TableCell>
 
-    const TableView = ({data, headers}) => (
+            <TableCell align="center" id={labelId}>{user.name}</TableCell>
+            <TableCell align="center">{user.email}</TableCell>
+            <TableCell align="center">{user.role}</TableCell>
+
+            <TableCell align="center">
+                <Stack direction="row" justifyContent="center" spacing={2}>
+                    <IconButton aria-label="delete" size="large" sx={{color: "button.success.main"}}>
+                        <EditIcon fontSize="inherit" />
+                    </IconButton>
+
+                    <IconButton aria-label="delete" size="large" sx={{color: "button.danger.main"}}>
+                        <DeleteOutlineIcon fontSize="inherit" />
+                    </IconButton>
+                </Stack>
+            </TableCell>
+
+        </TableRow>
+    );}
+
+    const TableView = ({data}) => (
         <TableContainer component={Paper}>
             <Table>
+
                 <TableHead>
                     <TableRow>
-                        {headers.map((h) => getTableCell(h))}
+                        <TableCell align="center">
+                            <Checkbox
+                            color="primary"
+                            indeterminate={selected.size > 0 && selected.size < users.length}
+                            checked={selected.size > 0 && selected.size === users.length}
+                            onClick={handleSelectAllClick}
+                            inputProps={{
+                              'aria-labelledby': "header",
+                            }}
+                            />
+                        </TableCell>
+
+                        <TableCell align="center">Name</TableCell>
+                        <TableCell align="center">Email</TableCell>
+                        <TableCell align="center">Role</TableCell>
+                        <TableCell align="center">Actions</TableCell>
                     </TableRow>
                 </TableHead>
+                
                 <TableBody>
-                    {data.map((d) => getTableRow(d, headers))}
+                    {data.map(getUserTableRow)}
                 </TableBody>
+
             </Table>
         </TableContainer>
     );
@@ -85,7 +176,6 @@ export default function UserList() {
         }
 
     }
-
 
     // Fetch Users for the first time 
     // No need of async here, since no promised returned
@@ -108,7 +198,7 @@ export default function UserList() {
             <Box
                 className="user-list-table-view-container"
             >
-                <TableView data={users} headers={headers} />
+                <TableView data={users}/>
             </Box>
 
             <Box
