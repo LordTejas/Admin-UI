7cacbd77d74382be987652704c80ea110909761f
LordTejas <tejas5225@gmail.com>
2023-04-20 20:24:25 +0530

    README: Updated readme file

--- frontend/README.md
+++ frontend/README.md
@@ -24,13 +24,20 @@ Note: You need npm installed in order to run application
 
 In the project directory, you can run:
 
-### `npm start`
+### `npm run start`
 
 Runs the app in the development mode.\
 Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
 
 The page will reload when you make changes.\
 You may also see any lint errors in the console.
+
+
+### `npm run test`
+
+Will run the test cases from available test suites
+
+Test suites are stored in **/frontend/src/__test__/\***
 
 
 ## Mobile Responsiveness
--- frontend/src/__tests__/UserList.test.js
+++ frontend/src/__tests__/UserList.test.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   act,
   render,
@@ -6,123 +6,118 @@ import {
   waitForElementToBeRemoved,
 } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import axios from 'axios';
+import axios from "axios";
 import MockAdapter from "axios-mock-adapter";
-import App from '../App';
-import config from '../config.json';
-
+import App from "../App";
+import config from "../config.json";
 
 /** User Event Handler object */
 const User = userEvent;
 
-/** 
+/**
  * Mocks the API methods to give mock data as response
  */
 const mock = new MockAdapter(axios);
 
-
 const usersResponse = [
   {
-  "id": "1",
-  "name": "Aaron Miles",
-  "email": "aaron@mailinator.com",
-  "role": "member"
+    id: "1",
+    name: "Aaron Miles",
+    email: "aaron@mailinator.com",
+    role: "member",
   },
   {
-  "id": "2",
-  "name": "Aishwarya Naik",
-  "email": "aishwarya@mailinator.com",
-  "role": "member"
+    id: "2",
+    name: "Aishwarya Naik",
+    email: "aishwarya@mailinator.com",
+    role: "member",
   },
   {
-  "id": "3",
-  "name": "Arvind Kumar",
-  "email": "arvind@mailinator.com",
-  "role": "admin"
+    id: "3",
+    name: "Arvind Kumar",
+    email: "arvind@mailinator.com",
+    role: "admin",
   },
   {
-  "id": "4",
-  "name": "Caterina Binotto",
-  "email": "caterina@mailinator.com",
-  "role": "member"
+    id: "4",
+    name: "Caterina Binotto",
+    email: "caterina@mailinator.com",
+    role: "member",
   },
   {
-  "id": "5",
-  "name": "Chetan Kumar",
-  "email": "chetan@mailinator.com",
-  "role": "member"
+    id: "5",
+    name: "Chetan Kumar",
+    email: "chetan@mailinator.com",
+    role: "member",
   },
   {
-  "id": "6",
-  "name": "Jim McClain",
-  "email": "jim@mailinator.com",
-  "role": "member"
+    id: "6",
+    name: "Jim McClain",
+    email: "jim@mailinator.com",
+    role: "member",
   },
   {
-  "id": "7",
-  "name": "Mahaveer Singh",
-  "email": "mahaveer@mailinator.com",
-  "role": "member"
+    id: "7",
+    name: "Mahaveer Singh",
+    email: "mahaveer@mailinator.com",
+    role: "member",
   },
   {
-  "id": "8",
-  "name": "Rahul Jain",
-  "email": "rahul@mailinator.com",
-  "role": "admin"
+    id: "8",
+    name: "Rahul Jain",
+    email: "rahul@mailinator.com",
+    role: "admin",
   },
   {
-  "id": "9",
-  "name": "Rizan Khan",
-  "email": "rizan@mailinator.com",
-  "role": "member"
+    id: "9",
+    name: "Rizan Khan",
+    email: "rizan@mailinator.com",
+    role: "member",
   },
   {
-  "id": "10",
-  "name": "Sarah Potter",
-  "email": "sarah@mailinator.com",
-  "role": "admin"
+    id: "10",
+    name: "Sarah Potter",
+    email: "sarah@mailinator.com",
+    role: "admin",
   },
   {
-  "id": "11",
-  "name": "Keshav Muddaiah",
-  "email": "keshav@mailinator.com",
-  "role": "member"
+    id: "11",
+    name: "Keshav Muddaiah",
+    email: "keshav@mailinator.com",
+    role: "member",
   },
   {
-  "id": "12",
-  "name": "Nita Ramesh",
-  "email": "nita@mailinator.com",
-  "role": "member"
+    id: "12",
+    name: "Nita Ramesh",
+    email: "nita@mailinator.com",
+    role: "member",
   },
   {
-  "id": "13",
-  "name": "Julia Hunstman",
-  "email": "julia@mailinator.com",
-  "role": "member"
+    id: "13",
+    name: "Julia Hunstman",
+    email: "julia@mailinator.com",
+    role: "member",
   },
   {
-  "id": "14",
-  "name": "Juan Alonso",
-  "email": "juan@mailinator.com",
-  "role": "admin"
+    id: "14",
+    name: "Juan Alonso",
+    email: "juan@mailinator.com",
+    role: "admin",
   },
   {
-  "id": "15",
-  "name": "Gabriel Montoya",
-  "email": "gabriel@mailinator.com",
-  "role": "admin"
+    id: "15",
+    name: "Gabriel Montoya",
+    email: "gabriel@mailinator.com",
+    role: "admin",
   },
 ];
 
 /** GET - users from the endPoint */
 mock.onGet(`${config.endpoint}`).reply(200, usersResponse);
 
-
 jest.useFakeTimers();
 
-
-describe('admin-ui tests', () => {
-
+describe("admin-ui tests", () => {
   beforeEach(async () => {
     // https://github.com/clarkbw/jest-localstorage-mock/issues/125
     jest.clearAllMocks();
@@ -131,7 +126,6 @@ describe('admin-ui tests', () => {
       render(<App />);
     });
   });
-
 
   /**
    * SearchBar Functionality Tests
@@ -143,34 +137,32 @@ describe('admin-ui tests', () => {
 
   test("2. Search Bar: Should be able to accept text", async () => {
     const searchInput = screen.getByPlaceholderText(/search/i);
-    
+
     // jest.runAllTimers();
     // await jest.setTimeout(5000);
 
     const searchText = "Tejas";
     await act(() => {
-        User.type(searchInput, searchText);
+      User.type(searchInput, searchText);
     });
 
     // console.log("Search: ", searchInput.getAttribute('value'));
 
-    expect(searchInput.getAttribute('value')).toEqual(searchText);
+    expect(searchInput.getAttribute("value")).toEqual(searchText);
   });
 
-  test('3. Table View: Check if the Headers are loaded', async () => {
-
+  test("3. Table View: Check if the Headers are loaded", async () => {
     // const headers = ["Name", "Email", "Role", "Actions"];
     const foundElements = screen.getAllByRole("columnheader");
 
     // Check if all 5 the headers are properly loaded
     expect(foundElements.length).toEqual(5);
-    
   });
 
-  test('4. Table View: Table Body is rendered', async () => {
+  test("4. Table View: Table Body is rendered", async () => {
     let container;
     await act(async () => {
-        ({container} = render(<App />));
+      ({ container } = render(<App />));
     });
 
     // Find if all selectable table rows are found
@@ -178,7 +170,7 @@ describe('admin-ui tests', () => {
     expect(tableBody).toBeInTheDocument();
   });
 
-  test('5. Table View: Table Rows are loaded', async () => {
+  test("5. Table View: Table Rows are loaded", async () => {
     // Wait for 5 seconds to render fetched data
     jest.runAllTimers();
 
@@ -191,25 +183,21 @@ describe('admin-ui tests', () => {
     expect(tableRows.length > 5).toBeTruthy();
   });
 
-
-  test('6. Table Actions Area: Check Delete Selected Button Exists', async () => {
+  test("6. Table Actions Area: Check Delete Selected Button Exists", async () => {
     const deleteButton = screen.getByText(/delete/i);
     expect(deleteButton).toBeInTheDocument();
   });
 
-  test('7. Table Actions Area: Check Pagination Control is available', async () => {
+  test("7. Table Actions Area: Check Pagination Control is available", async () => {
     const deleteButton = screen.getByLabelText(/navigation/i);
     expect(deleteButton).toBeInTheDocument();
   });
 
-  test('8. Table Actions Area: Check Row Limit Select is available', async () => {
+  test("8. Table Actions Area: Check Row Limit Select is available", async () => {
     const RowLimitSelect = screen.getByLabelText(/row limit/i);
     expect(RowLimitSelect).toBeInTheDocument();
   });
-  
-
 });
-
 
 /**
  * Better Create a seperate module to test the functionalities of each feature!
